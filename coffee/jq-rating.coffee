06e7bff192f250212392a459ced6af529f8bf82d
Olivier Bossel <olivier.bossel@esl-education.org>
2014-06-24 15:56:23 +0200

    init repo

---
+++ coffee/jq-rating.coffee
@@ -0,0 +1,335 @@
+do ->
+
+	# plugin name variable used to identify plugin
+	pluginName = 'jqRating'
+
+	# plugin class
+	class jqRating
+
+		# constructor
+		constructor : (item, options) ->
+
+			# set variables
+			@$this = $(item)
+			@settings =
+				value : 3.5
+				levelsClasses : ['xlow','low','medium','high','xhigh']
+				updateOn : 'click'		# define if the actual value will be updated on click or on hover (click | hover)
+				starsCount : 5
+				basedOn : 5
+				iconClass : 'fa fa-star'
+				editable : false
+				showGrade : true
+				onChange : null 		# callback when value change (params : value, api)
+			@$refs =
+				starsContainer : null 		# store the stars container if exist
+				base : null 			# store the html base stars element
+				hover : null 			# store the html hover stars element
+				grade : null 			# store the elements to update with the rating value
+				starsBase : null 		# store all the base stars
+				starsHover : null 		# store all the hover stars
+			@levelClass = null 			# store the applied level class to be able to remove it, etc...
+			@hoverValue = null			# store hoverValue
+
+			# init
+			@_init(options)
+
+		###
+		# Init
+		###
+		_init : (options) ->
+
+			# extend with inline settings
+			@_extendSettings(options)
+
+			# get the stars container
+			@$refs.starsContainer = @$this.find '[data-jq-rating-stars]:first'
+			@$refs.starsContainer = @$this if not @$refs.starsContainer.length
+
+			# get all the grades element to update the value
+			@$refs.grade = @$this.find '[data-jq-rating-grade]'
+
+			# generate the html
+			@_generateHtml()
+
+			# apply base css :
+			@_applyBaseCss()
+
+			# add events listeners
+			@_addEventsListeners()
+
+			# set the value
+			@setValue @settings.value
+
+		###
+		# Destroy
+		###
+		destroy : () ->
+
+			# remove events listeners
+			@$refs.starsBase.unbind 'mouseover'
+			@$refs.starsBase.unbind 'click'
+			@$refs.starsContainer.unbind 'mouseleave'
+
+			# remove the html elements
+			@$refs.starsContainer.empty()
+
+			# delete the references
+			@$refs.starsContainer = null
+			@$refs.base = null
+			@$refs.hover = null
+			@$refs.grade = null
+			@$refs.starsBase = null
+			@$refs.starsHover = null
+			@$refs.formField = null
+
+			# remove classes
+			@$this.removeClass 'jq-rating'
+			@$this.removeClass @levelClass if @levelClass
+			@$this.removeClass 'jq-rating--editable' if @isEditable()
+
+		###
+		# Update
+		###
+		update : () ->
+
+			# destroy
+			@destroy()
+
+			# init again
+			@_init()
+
+		###
+		# Generate html
+		###
+		_generateHtml : () ->
+
+			# add class to container
+			@$this.addClass 'jq-rating'
+
+			# add editable class if needed
+			@$this.addClass 'jq-rating--editable' if @isEditable()
+
+			# base
+			@$refs.base = $('<span class="jq-rating-group" />')
+
+			# hover
+			@$refs.hover = $('<span class="jq-rating-group--hover" />')
+
+			# generate the good number of stars
+			stars = []
+			for i in [0...@settings.starsCount]
+
+				stars.push ['<span class="jq-rating-star">'
+						'<i class="'+@settings.iconClass+'"></i>'
+					'</span>'].join('')
+
+			# append to html :
+			@$refs.starsContainer.append @$refs.base
+			          .append @$refs.hover
+
+			# append stars
+			@$refs.base.append $(stars.join(''))
+			@$refs.hover.append $(stars.join(''))
+
+			# get the stars references
+			@$refs.starsBase = @$refs.base.children()
+			@$refs.starsHover = @$refs.hover.children()
+
+		###
+		# Apply base css
+		###
+		_applyBaseCss : () ->
+
+			# apply css on stars container if exist
+			if @$refs.starsContainer.length
+				@$refs.starsContainer.css
+					position : 'relative' 
+					display : 'inline-block'
+
+			# stars group
+			@$refs.base.css
+				top : 0
+				left : 0
+			@$refs.hover.css
+				top : 0
+				left : 0
+
+		###
+		# Add events listeners
+		###
+		_addEventsListeners : () ->
+
+			@$this.find('input[data-jq-rating-grade], textarea[data-jq-rating-grade]').on 'change', (e) =>
+				 # grab the content
+				value = parseInt(e.currentTarget.value)
+
+				# secure value
+				value = 0 if value < 0
+				value = @settings.basedOn if value > @settings.basedOn
+
+				# set the value
+				@setValue value
+
+			# if editable, listen for hover on stars
+			if @isEditable()
+
+				# listen for hover on stars
+				@$refs.starsBase.bind 'mouseover', (e) =>
+
+					# index
+					index = $(e.currentTarget).index() + 1
+
+					# calculate value
+					value = @settings.basedOn / @settings.starsCount * index
+
+					# check if update the value on hover
+					if @settings.updateOn == 'hover'
+
+						# update the value
+						@setValue value
+
+					# if not, just update the temp value and render
+					else
+
+						# set temp value
+						@hoverValue = value
+
+						# render
+						@render()
+
+				# listen for click
+				@$refs.starsBase.bind 'click', (e) =>
+
+					# index
+					index = $(e.currentTarget).index() + 1
+
+					# set value
+					@setValue @settings.basedOn / @settings.starsCount * index
+
+
+				# listen for mouse out
+				@$refs.starsContainer.bind 'mouseleave', (e) =>
+					
+					# reset the hover value
+					@hoverValue = null
+
+					# render
+					@render()
+
+
+		###
+		# Set the rating value
+		###
+		setValue : (value) ->
+
+			# save the value
+			@value = value
+
+			# render
+			@render()
+
+			# update each grade
+			@$refs.grade.each (index, elm) =>
+
+				# check if is an input or textarea
+				switch elm.nodeName.toLowerCase()
+					when 'textarea', 'input'
+						# update value
+						$(elm).val @value
+					else
+						# update html
+						$(elm).html @value
+
+			# fire callbacks
+			@settings.onChange(@value, @) if @settings.onChange
+			@$this.trigger 'jqRating.change'
+
+		###
+		# Render
+		###
+		render : () ->
+
+			# get the value
+			value = @hoverValue or @value
+
+			# Calculate the width
+			width = 100 / @settings.basedOn * value
+
+			# set the width of the hover element
+			@$refs.hover.css
+				width : width + '%'
+
+			# manage classes
+			levelClassIdx = Math.round((@settings.levelsClasses.length / @settings.basedOn * value) - 1)
+			levelClassIdx = 0 if levelClassIdx < 0
+			levelClassIdx = @settings.levelsClasses.length-1 if levelClassIdx >= @settings.levelsClasses.length
+
+			@$this.removeClass @levelClass if @levelClass
+			@levelClass = 'jq-rating--'+@settings.levelsClasses[levelClassIdx]
+			@$this.addClass @levelClass
+
+		###
+		# Check if is editable or not
+		###
+		isEditable : () ->
+			editable = @settings.editable and @settings.editable == 'true'
+			return editable
+
+		###
+		# Extend settings
+		###
+		_extendSettings : (options) ->
+
+			# extend options
+			@settings = $.extend @settings, options, true if options?
+
+			# extend options with inline ones :
+			$.each @$this.get(0).attributes, (index, attr) =>
+
+				# process name
+				name = attr.name
+				name = name.substr('data-jq-rating-'.length)
+				name = name.replace /-([a-z])/g, (g) ->
+					return g[1].toUpperCase();
+
+				return if not name
+
+				# check if is a settings like this
+				@settings[name] = attr.value if @settings[name] != undefined
+
+
+	###
+	# jQuery controller :
+	###
+	$.fn.jqRating = (method) ->
+
+		if jqRating.prototype[method]
+
+			# store arguments to use later
+			args = Array.prototype.slice.call( arguments, 1 )
+
+			# apply on each elements
+			this.each () ->
+				# get the plugin instance from element
+				plugin = $(this).data pluginName+'_api'
+				# call the method on plugin if exist
+				plugin[method].apply( plugin, args)
+
+		else if typeof method == 'object' or not method
+
+			this.each () ->
+
+				$this = $(this)
+
+				# do nothing if already inited
+				return if $this.data(pluginName+'_api')? and $this.data(pluginName+'_api') != ''
+
+				# make initialisation of plugin
+				plugin = new jqRating this, arguments[0]
+
+				# save plugin in element
+				$(this).data pluginName+'_api', plugin
+
+		else
+			$.error 'Method ' +  method + ' does not exist on jQuery.jqRating'	
---
+++ js/jq-rating.js
@@ -0,0 +1,301 @@
+(function() {
+  var jqRating, pluginName;
+  pluginName = 'jqRating';
+  jqRating = (function() {
+    function jqRating(item, options) {
+      this.$this = $(item);
+      this.settings = {
+        value: 3.5,
+        levelsClasses: ['xlow', 'low', 'medium', 'high', 'xhigh'],
+        updateOn: 'click',
+        starsCount: 5,
+        basedOn: 5,
+        iconClass: 'fa fa-star',
+        editable: false,
+        showGrade: true,
+        onChange: null
+      };
+      this.$refs = {
+        starsContainer: null,
+        base: null,
+        hover: null,
+        grade: null,
+        starsBase: null,
+        starsHover: null
+      };
+      this.levelClass = null;
+      this.hoverValue = null;
+      this._init(options);
+    }
+
+
+    /*
+    		 * Init
+     */
+
+    jqRating.prototype._init = function(options) {
+      this._extendSettings(options);
+      this.$refs.starsContainer = this.$this.find('[data-jq-rating-stars]:first');
+      if (!this.$refs.starsContainer.length) {
+        this.$refs.starsContainer = this.$this;
+      }
+      this.$refs.grade = this.$this.find('[data-jq-rating-grade]');
+      this._generateHtml();
+      this._applyBaseCss();
+      this._addEventsListeners();
+      return this.setValue(this.settings.value);
+    };
+
+
+    /*
+    		 * Destroy
+     */
+
+    jqRating.prototype.destroy = function() {
+      this.$refs.starsBase.unbind('mouseover');
+      this.$refs.starsBase.unbind('click');
+      this.$refs.starsContainer.unbind('mouseleave');
+      this.$refs.starsContainer.empty();
+      this.$refs.starsContainer = null;
+      this.$refs.base = null;
+      this.$refs.hover = null;
+      this.$refs.grade = null;
+      this.$refs.starsBase = null;
+      this.$refs.starsHover = null;
+      this.$refs.formField = null;
+      this.$this.removeClass('jq-rating');
+      if (this.levelClass) {
+        this.$this.removeClass(this.levelClass);
+      }
+      if (this.isEditable()) {
+        return this.$this.removeClass('jq-rating--editable');
+      }
+    };
+
+
+    /*
+    		 * Update
+     */
+
+    jqRating.prototype.update = function() {
+      this.destroy();
+      return this._init();
+    };
+
+
+    /*
+    		 * Generate html
+     */
+
+    jqRating.prototype._generateHtml = function() {
+      var i, stars, _i, _ref;
+      this.$this.addClass('jq-rating');
+      if (this.isEditable()) {
+        this.$this.addClass('jq-rating--editable');
+      }
+      this.$refs.base = $('<span class="jq-rating-group" />');
+      this.$refs.hover = $('<span class="jq-rating-group--hover" />');
+      stars = [];
+      for (i = _i = 0, _ref = this.settings.starsCount; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+        stars.push(['<span class="jq-rating-star">', '<i class="' + this.settings.iconClass + '"></i>', '</span>'].join(''));
+      }
+      this.$refs.starsContainer.append(this.$refs.base).append(this.$refs.hover);
+      this.$refs.base.append($(stars.join('')));
+      this.$refs.hover.append($(stars.join('')));
+      this.$refs.starsBase = this.$refs.base.children();
+      return this.$refs.starsHover = this.$refs.hover.children();
+    };
+
+
+    /*
+    		 * Apply base css
+     */
+
+    jqRating.prototype._applyBaseCss = function() {
+      if (this.$refs.starsContainer.length) {
+        this.$refs.starsContainer.css({
+          position: 'relative',
+          display: 'inline-block'
+        });
+      }
+      this.$refs.base.css({
+        top: 0,
+        left: 0
+      });
+      return this.$refs.hover.css({
+        top: 0,
+        left: 0
+      });
+    };
+
+
+    /*
+    		 * Add events listeners
+     */
+
+    jqRating.prototype._addEventsListeners = function() {
+      this.$this.find('input[data-jq-rating-grade], textarea[data-jq-rating-grade]').on('change', (function(_this) {
+        return function(e) {
+          var value;
+          value = parseInt(e.currentTarget.value);
+          if (value < 0) {
+            value = 0;
+          }
+          if (value > _this.settings.basedOn) {
+            value = _this.settings.basedOn;
+          }
+          return _this.setValue(value);
+        };
+      })(this));
+      if (this.isEditable()) {
+        this.$refs.starsBase.bind('mouseover', (function(_this) {
+          return function(e) {
+            var index, value;
+            index = $(e.currentTarget).index() + 1;
+            value = _this.settings.basedOn / _this.settings.starsCount * index;
+            if (_this.settings.updateOn === 'hover') {
+              return _this.setValue(value);
+            } else {
+              _this.hoverValue = value;
+              return _this.render();
+            }
+          };
+        })(this));
+        this.$refs.starsBase.bind('click', (function(_this) {
+          return function(e) {
+            var index;
+            index = $(e.currentTarget).index() + 1;
+            return _this.setValue(_this.settings.basedOn / _this.settings.starsCount * index);
+          };
+        })(this));
+        return this.$refs.starsContainer.bind('mouseleave', (function(_this) {
+          return function(e) {
+            _this.hoverValue = null;
+            return _this.render();
+          };
+        })(this));
+      }
+    };
+
+
+    /*
+    		 * Set the rating value
+     */
+
+    jqRating.prototype.setValue = function(value) {
+      this.value = value;
+      this.render();
+      this.$refs.grade.each((function(_this) {
+        return function(index, elm) {
+          switch (elm.nodeName.toLowerCase()) {
+            case 'textarea':
+            case 'input':
+              return $(elm).val(_this.value);
+            default:
+              return $(elm).html(_this.value);
+          }
+        };
+      })(this));
+      if (this.settings.onChange) {
+        this.settings.onChange(this.value, this);
+      }
+      return this.$this.trigger('jqRating.change');
+    };
+
+
+    /*
+    		 * Render
+     */
+
+    jqRating.prototype.render = function() {
+      var levelClassIdx, value, width;
+      value = this.hoverValue || this.value;
+      width = 100 / this.settings.basedOn * value;
+      this.$refs.hover.css({
+        width: width + '%'
+      });
+      levelClassIdx = Math.round((this.settings.levelsClasses.length / this.settings.basedOn * value) - 1);
+      if (levelClassIdx < 0) {
+        levelClassIdx = 0;
+      }
+      if (levelClassIdx >= this.settings.levelsClasses.length) {
+        levelClassIdx = this.settings.levelsClasses.length - 1;
+      }
+      if (this.levelClass) {
+        this.$this.removeClass(this.levelClass);
+      }
+      this.levelClass = 'jq-rating--' + this.settings.levelsClasses[levelClassIdx];
+      return this.$this.addClass(this.levelClass);
+    };
+
+
+    /*
+    		 * Check if is editable or not
+     */
+
+    jqRating.prototype.isEditable = function() {
+      var editable;
+      editable = this.settings.editable && this.settings.editable === 'true';
+      return editable;
+    };
+
+
+    /*
+    		 * Extend settings
+     */
+
+    jqRating.prototype._extendSettings = function(options) {
+      if (options != null) {
+        this.settings = $.extend(this.settings, options, true);
+      }
+      return $.each(this.$this.get(0).attributes, (function(_this) {
+        return function(index, attr) {
+          var name;
+          name = attr.name;
+          name = name.substr('data-jq-rating-'.length);
+          name = name.replace(/-([a-z])/g, function(g) {
+            return g[1].toUpperCase();
+          });
+          if (!name) {
+            return;
+          }
+          if (_this.settings[name] !== void 0) {
+            return _this.settings[name] = attr.value;
+          }
+        };
+      })(this));
+    };
+
+    return jqRating;
+
+  })();
+
+  /*
+  	 * jQuery controller :
+   */
+  return $.fn.jqRating = function(method) {
+    var args;
+    if (jqRating.prototype[method]) {
+      args = Array.prototype.slice.call(arguments, 1);
+      return this.each(function() {
+        var plugin;
+        plugin = $(this).data(pluginName + '_api');
+        return plugin[method].apply(plugin, args);
+      });
+    } else if (typeof method === 'object' || !method) {
+      return this.each(function() {
+        var $this, plugin;
+        $this = $(this);
+        if (($this.data(pluginName + '_api') != null) && $this.data(pluginName + '_api') !== '') {
+          return;
+        }
+        plugin = new jqRating(this, arguments[0]);
+        return $(this).data(pluginName + '_api', plugin);
+      });
+    } else {
+      return $.error('Method ' + method + ' does not exist on jQuery.jqRating');
+    }
+  };
+})();
+
+//# sourceMappingURL=jq-rating.js.map
